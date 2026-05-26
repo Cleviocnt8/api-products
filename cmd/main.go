bb6b2b698b8b3924e0585ac2e07569a24ea52a45
Clevio Tivane <cleviocnt8@gmail.com>
2026-05-26 14:20:14 +0200

    Base de dados

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"go-api/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	productController := controller.NewProductController()
+
+	server.GET("/ping", func(ctx *gin.Context){
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/products", productController.GetProducts)
+	server.Run(":8000")
+}
